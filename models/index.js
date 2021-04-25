308b777f57473d9ecd537303953414e6f87ccc9e
Breedlove13 <b.breedlove@outlook.com>
2021-04-25 12:22:49 -0700

    associations set up

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: 'Category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongsToMany(Tags, {
+  foreignKey: 'Product_id',
+})
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  foreignKey: 'Product_id'
+})
+
 
 module.exports = {
   Product,
